796b7a3ecc52ddc526200f4ebc967f6e5eeececb
ice <1058232569@qq.com>
2016-11-03 11:13:04 +0800

    project

---
+++ src/main/Class.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * Created by huangbingjing on 16/11/1.
+ */
+public class Class {
+    public static void main(String[] args) {
+        System.out.println("sssss");
+
+    }
+}
---
+++ src/main/Class1.java
@@ -0,0 +1,7 @@
+package main;
+
+/**
+ * Created by huangbingjing on 16/11/1.
+ */
+public class Class1 {
+}
